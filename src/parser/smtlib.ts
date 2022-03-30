d865837a389600b265cae999483e4f8bceb6c81c
Michal Pospěch <michal.pospech@gmail.com>
2022-03-30 21:23:51 +0000

    SMTLIB parsing

---
+++ src/parser/smtlib.ts
@@ -0,0 +1,74 @@
+import * as P from "parsimmon";
+
+interface Variable {
+  name: string;
+}
+
+interface Conjunction {
+  f1: NNFFormula;
+  f2: NNFFormula;
+}
+
+interface Negation {
+  var: Variable;
+}
+
+interface Disjunction {
+  f1: NNFFormula;
+  f2: NNFFormula;
+}
+
+type NNFFormula = Negation | Disjunction | Conjunction | Variable;
+
+export const language = P.createLanguage<{
+  formula: NNFFormula;
+  and: Conjunction;
+  or: Disjunction;
+  variable: Variable;
+  negation: Negation;
+  lParen: string;
+  rParen: string;
+}>({
+  formula: (r) => P.alt(r.and, r.or, r.variable, r.negation),
+  and: (r) =>
+    P.seqObj<Conjunction>(
+      r.lParen,
+      P.whitespace,
+      P.string("and"),
+      ["f1", r.formula],
+      P.whitespace,
+      ["f2", r.formula],
+      P.whitespace,
+      r.rParen
+    ),
+  or: (r) =>
+    P.seqObj<Disjunction>(
+      r.lParen,
+      P.whitespace,
+      P.string("or"),
+      ["f1", r.formula],
+      P.whitespace,
+      ["f2", r.formula],
+      P.whitespace,
+      r.rParen
+    ),
+  variable: () =>
+    P.regex(RegExp("[a-zA-Z][a-zA-Z0-9]*")).map<Variable>(
+      (s) =>
+        <Variable>{
+          name: s,
+        }
+    ),
+  negation: (r) =>
+    P.seqObj<Negation>(
+      r.lParen,
+      P.whitespace,
+      P.string("not"),
+      P.whitespace,
+      ["var", r.variable],
+      P.whitespace,
+      r.rParen
+    ),
+  lParen: () => P.string("("),
+  rParen: () => P.string(")"),
+});
