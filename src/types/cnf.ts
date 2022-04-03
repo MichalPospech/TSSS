e5abbcf94c407390a325791e10b87b3d4c81b870
Michal Pospěch <michal.pospech@gmail.com>
2022-04-03 21:07:04 +0000

    Helpers

--- src/types/cnf.ts
+++ src/types/cnf.ts
@@ -1,9 +1,20 @@
 import { Negation, Variable } from "./common";
 
 export interface Clause {
-  variables: Array<Negation | Variable>;
+  literals: Array<Negation | Variable>;
 }
 
 export interface CNFFormula {
   clauses: Array<Clause>;
+}
+
+export function createClause(...literals: (Negation | Variable)[]): Clause {
+  return {
+    literals: Array.from(literals),
+  };
+}
+export function createCNFFormula(...clauses: Clause[]): CNFFormula {
+  return {
+    clauses: Array.from(clauses),
+  };
 }
--- src/types/common.ts
+++ src/types/common.ts
@@ -6,3 +6,17 @@ export interface Negation {
   var: Variable;
   type: "not";
 }
+
+export function createVariable(name: string): Variable {
+  return {
+    type: "var",
+    name: name,
+  };
+}
+
+export function createNegation(variable: Variable): Negation {
+  return {
+    type: "not",
+    var: variable,
+  };
+}
